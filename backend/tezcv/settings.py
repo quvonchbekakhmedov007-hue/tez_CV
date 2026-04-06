dec4386e24ff5029e2ddcd311ae0800b1c7d9b59
Quvonchbek Axmedov <quvonchbekakhmedov007@gmail.com>
2026-04-06 23:11:37 +0500

    initial commit

--- backend/tezcv/settings.py
+++ backend/tezcv/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-tezcv-secret-key-change-in-production-2024')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 ALLOWED_HOSTS = ['*']
 
